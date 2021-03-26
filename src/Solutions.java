ab93f07881cfb799328501c50bc88737a5c78e6f
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-03-26 22:49:54 +0530

    Factorial of number

--- src/Solutions.java
+++ src/Solutions.java
@@ -1,5 +1,14 @@
 public class Solutions {
+    public int factorial(int number){
+        int result = 1;
+        while(number!=0){
+            result = result*number;
+            number--;
+        }
+        return result;
+    }
     public static void main(String[] args) {
-        System.out.println("hello");
+        Solutions my = new Solutions();
+        System.out.println(my.factorial(5));
     }
 }
