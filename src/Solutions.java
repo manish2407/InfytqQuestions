6f61cbc98a4da13fc432687a12b4a605f41a2c93
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-03-26 22:57:23 +0530

    to print the geometric Sequence of number

--- src/Solutions.java
+++ src/Solutions.java
@@ -1,4 +1,5 @@
 public class Solutions {
+    /*1. factorial of number
     public int factorial(int number){
         int result = 1;
         while(number!=0){
@@ -10,5 +11,17 @@ public class Solutions {
     public static void main(String[] args) {
         Solutions my = new Solutions();
         System.out.println(my.factorial(5));
+    }*/
+    public void geometricSequence(int number){
+        System.out.print(1);
+        int result=1;
+        for (int i =1;i<number;i++){
+            result=result*2;
+            System.out.print(","+result);
+        }
+    }
+    public static void main(String[] args) {
+        Solutions my = new Solutions();
+        my.geometricSequence(5);
     }
 }
