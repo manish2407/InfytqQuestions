752873766e9f389ceb56f167276ac7e84b768b37
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-03-26 23:06:51 +0530

    to print the sum of two number if number are equal

--- src/Solutions.java
+++ src/Solutions.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Solutions {
     /*1. factorial of number
     public int factorial(int number){
@@ -12,6 +14,7 @@ public class Solutions {
         Solutions my = new Solutions();
         System.out.println(my.factorial(5));
     }*/
+    /*2. To print the geometric Sequence of number
     public void geometricSequence(int number){
         System.out.print(1);
         int result=1;
@@ -23,5 +26,16 @@ public class Solutions {
     public static void main(String[] args) {
         Solutions my = new Solutions();
         my.geometricSequence(5);
+    }*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        if(a==b){
+            System.out.println(a+b);
+        }
+        else{
+            System.out.println((a+b)*2);
+        }
     }
 }
